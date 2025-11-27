e8f862860e38cdea9e6a2afdc68ac722a6f8c261
Marco M <mmontero80802@ufide.ac.cr>
2025-11-27 10:21:16 -0600

    feat CRUD: implement delete and update methods for products

--- Backend/controllers/productos.py
+++ Backend/controllers/productos.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, session
-from services.productos_service import productos_todos, variaciones
+from services.productos_service import productos_todos, variaciones, insertar_producto, borrar_producto, modificar_producto
 from services.producto_service import producto_unico
+from services.categoria_service import traerCategorias
 
 productos_bp = Blueprint("productos", __name__)
 
@@ -16,3 +17,44 @@ def producto_variacion(id):
     variaciones_producto = variaciones(id)
 
     return render_template("productoVariaciones.html", producto = producto, variaciones = variaciones_producto)
+
+@productos_bp.route("/agregarproducto", methods = ["GET", "POST"])
+def agregar_producto():
+    all_categorias = traerCategorias()
+
+    if request.method == "POST":
+        item = {
+            "nombre": request.form["nombre"],
+            "descripcion": request.form["descripcion"],
+            "id_categoria": request.form["id_categoria"],
+        }
+
+        insertar_producto(item)
+        return redirect("/productos")
+
+    else:
+        return render_template("agregar_producto.html", categorias = all_categorias)
+
+@productos_bp.route("/eliminarproducto/<int:id>")
+def eliminar_producto(id):
+    borrar_producto(id=id)
+
+    return redirect("/productos")
+
+@productos_bp.route("/editarproducto/<int:id>", methods = ["GET", "POST"])
+def editar_producto(id):
+    all_categorias = traerCategorias()
+    producto_solo = producto_unico(id=id)
+
+    if request.method == "POST":
+        producto = {
+            "nombre": request.form["nombre"],
+            "descripcion": request.form["descripcion"],
+            "id_categoria": request.form["id_categoria"]
+        }
+
+        modificar_producto(id=id, item=producto)
+        return redirect("/productos")
+
+    else:
+        return render_template("editar_producto.html", producto = producto_solo, categorias = all_categorias)
--- Backend/services/productos_service.py
+++ Backend/services/productos_service.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import joinedload
+from main import db
 
 from models.models import Producto, ProductoVariacion
 
@@ -7,3 +8,31 @@ def productos_todos():
 
 def variaciones(id):
     return ProductoVariacion.query.filter_by(id_producto=id).all()
+
+def insertar_producto(producto):
+    nuevo_producto = Producto(
+        nombre = producto["nombre"],
+        descripcion = producto["descripcion"],
+        id_categoria = producto["id_categoria"])
+    
+    db.session.add(nuevo_producto)
+    db.session.commit()
+    
+def producto(id):
+    return Producto.query.filter_by(id_producto=id).first_or_404()
+
+def borrar_producto(id):
+    producto_borrar = producto(id)
+
+    if producto:
+        db.session.delete(producto_borrar)
+        db.session.commit()
+
+def modificar_producto(id, item):
+    producto_a_modificar = producto(id=id)
+
+    producto_a_modificar.nombre = item["nombre"]
+    producto_a_modificar.descripcion = item["descripcion"]
+    producto_a_modificar.id_categoria = item["id_categoria"]
+
+    db.session.commit()
