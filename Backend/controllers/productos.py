69bfee9071e2ca437925ea9cb6f63e0924b585ca
Marco M <mmontero80802@ufide.ac.cr>
2025-11-27 13:00:21 -0600

    feat add CRUD for product variations

--- Backend/controllers/productos.py
+++ Backend/controllers/productos.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, session
-from services.productos_service import productos_todos, variaciones, insertar_producto, borrar_producto, modificar_producto
+from services.productos_service import productos_todos, variaciones, insertar_producto, borrar_producto, modificar_producto, insertar_variacion, borrar_variacion, variacion, modificar_variacion
 from services.producto_service import producto_unico
 from services.categoria_service import traerCategorias
 
@@ -10,13 +10,6 @@ def productos():
     all_productos = productos_todos()
 
     return render_template("productos.html", productos = all_productos)
-
-@productos_bp.route("/productovariacion/<int:id>")
-def producto_variacion(id):
-    producto = producto_unico(id)
-    variaciones_producto = variaciones(id)
-
-    return render_template("productoVariaciones.html", producto = producto, variaciones = variaciones_producto)
 
 @productos_bp.route("/agregarproducto", methods = ["GET", "POST"])
 def agregar_producto():
@@ -58,3 +51,51 @@ def editar_producto(id):
 
     else:
         return render_template("editar_producto.html", producto = producto_solo, categorias = all_categorias)
+    
+@productos_bp.route("/productovariacion/<int:id>")
+def producto_variacion(id):
+    producto = producto_unico(id)
+    variaciones_producto = variaciones(id)
+
+    return render_template("productoVariaciones.html", producto = producto, variaciones = variaciones_producto)
+
+@productos_bp.route("/agregarvariacion/<int:id>", methods = ["GET", "POST"])
+def agregar_variacion(id):
+    producto_base = producto_unico(id)
+
+    if request.method == "POST":
+         print(request.form)
+         variacion = {
+            "id_producto": request.form["id_producto"],
+            "talla": request.form["talla"],
+            "stock": request.form["stock"],
+            "precio": request.form["precio"]
+         }
+         insertar_variacion(variacion)
+         return redirect("/productovariacion/"+str(id))
+    
+    else: 
+        return render_template("agregar_variacion.html", producto = producto_base)
+
+@productos_bp.route("/editarvariacion/<int:id>", methods = ["GET", "POST"])
+def editar_variacion(id):
+    variacion_unica = variacion(id=id)
+
+    if request.method == "POST":
+        variacion_modificada = {
+            "id_producto": request.form["id_producto"],
+            "talla" : request.form["talla"],
+            "stock" : request.form["stock"],
+            "precio" : request.form["precio"]
+        }
+        modificar_variacion(id=id, producto=variacion_modificada)
+        return redirect("/productos")
+    
+    else:
+        return render_template("editar_variacion.html", variacion = variacion_unica)
+
+
+@productos_bp.route("/eliminarvariacion/<int:id>")
+def eliminar_variacion(id):
+    borrar_variacion(id=id)
+    return redirect("/productos")
--- Backend/services/productos_service.py
+++ Backend/services/productos_service.py
@@ -6,9 +6,6 @@ from models.models import Producto, ProductoVariacion
 def productos_todos():
     return Producto.query.options(joinedload(Producto.tallas))
 
-def variaciones(id):
-    return ProductoVariacion.query.filter_by(id_producto=id).all()
-
 def insertar_producto(producto):
     nuevo_producto = Producto(
         nombre = producto["nombre"],
@@ -34,5 +31,39 @@ def modificar_producto(id, item):
     producto_a_modificar.nombre = item["nombre"]
     producto_a_modificar.descripcion = item["descripcion"]
     producto_a_modificar.id_categoria = item["id_categoria"]
+
+    db.session.commit()
+
+def variaciones(id):
+    return ProductoVariacion.query.filter_by(id_producto=id).all()
+
+def insertar_variacion(variacion):
+    nueva_variacion = ProductoVariacion(
+        id_producto = variacion["id_producto"],
+        talla = variacion["talla"],
+        stock = variacion["stock"],
+        precio = variacion["precio"]
+    )
+
+    db.session.add(nueva_variacion)
+    db.session.commit()
+
+def variacion(id):
+    return ProductoVariacion.query.filter_by(id_variacion=id).first_or_404()
+
+def borrar_variacion(id):
+    variacion_borrar = variacion(id=id)
+
+    if variacion:
+        db.session.delete(variacion_borrar)
+        db.session.commit()
+
+def modificar_variacion(id, producto):
+    variacion_a_modificar = variacion(id)
+
+    variacion_a_modificar.id_producto = producto["id_producto"]
+    variacion_a_modificar.talla = producto["talla"]
+    variacion_a_modificar.stock = producto["stock"]
+    variacion_a_modificar.precio = producto["precio"]
 
     db.session.commit()
